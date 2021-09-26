671bd689e9cc685c8cfea4afd9f534167c08d22d
franck-armand <52790721+franck225-coder@users.noreply.github.com>
2021-09-26 22:10:45 +0800

    scalar

---
+++ NumPy.py
@@ -0,0 +1,14 @@
+# Set up
+import numpy as np
+
+# we can set seed in order to have the same random value
+seed = np.random.seed(seed=1234)
+
+# scalar
+x = np.array(6)
+print("x: ",x)
+print("x ndim: ",x.ndim) # number of dimensions
+print("x shape: ",x.shape) # number of shape
+print("x size: ",x.size) # number of size
+print("x dtype: ",x.dtype) # number of data type
+
