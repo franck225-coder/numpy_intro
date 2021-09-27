cddfc30fa6a77c5c24eac88015570db15d9aee93
franck-armand <52790721+franck225-coder@users.noreply.github.com>
2021-09-27 16:20:23 +0800

    matrix

--- NumPy.py
+++ NumPy.py
@@ -8,14 +8,22 @@ seed = np.random.seed(seed=1234)
 x = np.array(6)
 print("x: ",x)
 print("x ndim: ",x.ndim) # number of dimensions
-print("x shape: ",x.shape) # number of shape
+print("x shape: ",x.shape) # dimension
 print("x size: ",x.size) # number of size
-print("x dtype: ",x.dtype) # number of data type
+print("x dtype: ",x.dtype) # data type
 
 # vector
 x = np.array([2.4, 1.1, 1.7])
 print("x: ",x)
 print("x ndim: ",x.ndim) # number of dimensions
-print("x shape: ",x.shape) # number of shape
+print("x shape: ",x.shape) # dimension
 print("x size: ",x.size) # number of size
-print("x dtype: ",x.dtype) # number of data type
+print("x dtype: ",x.dtype) # data type
+
+# matrix
+x = np.array([[2,5], [3,9]])
+print("x ", x)
+print("x ndim ", x.ndim) # number of dimensions
+print("x shape ", x.shape) # dimension
+print("x size ", x.size) # size of elements
+print("x dtype ", x.dtype) # data type
